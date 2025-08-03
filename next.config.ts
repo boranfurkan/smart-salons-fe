3da58bc694178ef61b9a243f9edc8267de290301
boranfurkan <furkanboran123@gmail.com>
2025-08-03 14:47:21 +0300

    fix: image configs

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
       bodySizeLimit: '10mb',
     },
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
   api: {
     bodyParser: {
       sizeLimit: '10mb',

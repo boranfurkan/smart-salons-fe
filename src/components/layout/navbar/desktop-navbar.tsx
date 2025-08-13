2a6ce36aa43b8ee9ef28e33e874cce5758292fc7
boranfurkan <furkanboran123@gmail.com>
2025-08-13 23:05:57 +0300

    fix: desktop navbar

--- src/components/layout/navbar/desktop-navbar.tsx
+++ src/components/layout/navbar/desktop-navbar.tsx
@@ -39,9 +39,9 @@ export function DesktopNavbar({
   const catRef = useRef<HTMLDivElement | null>(null);
 
   return (
-    <nav className="fixed left-1/2 top-4 z-50 hidden w-fit -translate-x-1/2 items-center gap-4 rounded-lg border border-neutral-200/70 bg-white/90 px-2 py-2 text-sm text-neutral-600 backdrop-blur-md shadow-md md:flex">
+    <nav className="fixed left-1/2 top-4 z-50 hidden w-fit -translate-x-1/2 items-center gap-4 rounded-lg border border-neutral-900/50 bg-white/90 px-2 py-2 text-sm text-black backdrop-blur-md shadow-md md:flex">
       <LogoNavbar />
-      <div className="mx-1 h-5 w-px bg-neutral-300/60" />
+      <div className="mx-1 h-5 w-px bg-neutral-900/50" />
 
       <NavLink href="/">Home</NavLink>
 
@@ -61,7 +61,7 @@ export function DesktopNavbar({
       </div>
 
       <NavLink href="/products">All Products</NavLink>
-      <div className="mx-1 h-5 w-px bg-neutral-300/60" />
+      <div className="mx-1 h-5 w-px bg-neutral-900/50" />
       <div className="flex items-center gap-2">
         {/* Right-side actions inside bar */}
 

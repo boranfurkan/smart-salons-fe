03bde8ff1a44df26d24999189e692f4bc9692a25
boranfurkan <furkanboran123@gmail.com>
2025-08-14 17:36:10 +0300

    fix: remove unused

--- src/components/shared/auth/index.ts
+++ src/components/shared/auth/index.ts
@@ -1,4 +1,3 @@
-export { AuthLayout } from './auth-layout';
 export { ModernAuthLayout } from './modern-auth-layout';
 export { PasswordInput } from './password-input';
 export { FormField, FormError, FormSuccess } from './form-components';
